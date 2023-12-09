37391d1d3eeb5eccd66d35cf27e10e03d0c44ac2
Fernilab <fgarcia@fernilab.com>
2023-12-08 18:01:05 -0600

    Move to pyproject

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-import setuptools
-
-setup(name='shmgpsd',
-      version='0.2.5',
-      description='GPSD shared memory tool',
-      package_dir={'':'shmgpsd'},
-      py_modules=['shmgpsd'],
-      )
